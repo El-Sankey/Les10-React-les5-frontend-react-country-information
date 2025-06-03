4911905db2d56f94a1ca94d07cda5f64300e4068
El-Sankey <i.a.rivashernandez@gmail.com>
2025-06-04 01:30:24 +0200

    header gemaakt, img opgeload en een h1 gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,15 @@ function App() {
 
     return (
         <>
-            Maak je applicatie hier!
+            <header>
+                <div>
+                    <img src="src/assets/world_map.png" alt="wereld-kaart"/>
+                    <button className="btn" type="submit">clik me</button>
+                    <h1 className="h1">world regions</h1>
+                </div>
+            </header>
+            <main>
+            </main>
         </>
     )
 }
